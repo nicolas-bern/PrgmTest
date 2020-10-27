d806facaefe9c69c7b5faa059015106c0dd34b99
nicolas.bern <nicolas.bern@epsi.fr>
2020-10-27 15:09:03 +0100

    TP déclaration

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,21 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args){
+        System.out.println("Hello world !");
+        byte b = 30;
+        float f = 1.5f;
+        short s = 32000;
+        double d = 1000000;
+        int i = 66;
+        boolean bo = false;
+        long l = -123_456_789L;
+        String st = "Jolie chaîne";
+
+        System.out.println("Byte = " + b);
+        System.out.println("Boulean = " + bo);
+        System.out.println("Long = " + l);
+        System.out.println("String = " + st);
+    }
+
+}
