ba3176bddeb5d6c19c3f1891f30555cb4636a6d9
nicolas.bern <nicolas.bern@epsi.fr>
2020-10-27 18:36:08 +0100

    Légères modif

--- src/fr/boucles/ExerciceBoucleBase.java
+++ src/fr/boucles/ExerciceBoucleBase.java
@@ -4,22 +4,26 @@ public class ExerciceBoucleBase {
     public static void main(String[] args) {
 
         //Tous les nombres de 1 à 10
+        System.out.println(("Tous les nombres de 1 à 10 :"));
         for (int i = 1; i < 11; i++) {
             System.out.println(i);
         }
 
         //20 fois nom et prénom
+        System.out.println("20 fois nom et prénom");
         for (int i = 0; i < 20; i++) {
             System.out.println("Nicolas Bern");
         }
 
 
         //Tous les éléments pairs de 2 à 100
+        System.out.println("Tous les éléments pairs de 2 à 100");
         for (int i = 1; i < 101; i++) {
             if(i % 2 == 0) System.out.println((i));
         }
 
         //Tous les éléments impairs de 1 à 99
+        System.out.println("Tous les éléments impairs de 1 à 99");
         for (int i = 1; i < 101; i++) {
             if (i % 2 != 0) System.out.println((i));
         }
--- src/fr/boucles/ExerciceBoucleEtTests.java
+++ src/fr/boucles/ExerciceBoucleEtTests.java
@@ -6,36 +6,42 @@ public class ExerciceBoucleEtTests {
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
 
         //Afficher l'ensemble des éléments du tableau grâce à une boucle
+        System.out.println("Array :");
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
 
 
         //Afficher l'ensemble des éléments dans l'ordre inverse
+        System.out.println("Tableau inversé :");
         for (int i = array.length-1; i > -1; i--) {
             System.out.println(array[i]);
         }
 
 
         //Afficher que les entiers supérieurs à 3
+        System.out.println("Entiers supérieurs à 3 :");
         for (int i = 0; i < array.length; i++) {
             if(array[i] > 3) System.out.println(array[i]);
         }
 
 
         //Afficher que les entiers pairs
+        System.out.println("Entiers pairs :");
         for (int i = 0; i < array.length; i++) {
             if(array[i] % 2 == 0) System.out.println(array[i]);
         }
 
 
         //Afficher que les index pairs
+        System.out.println("Index pairs :");
         for (int i = 0; i < array.length; i++) {
             if(array[i] % 2 == 0) System.out.println(i);
         }
 
 
-        //Afficher que les entiers pairs
+        //Afficher que les entiers impairs
+        System.out.println("Entiers impairs :");
         for (int i = 0; i < array.length; i++) {
             if(array[i] % 2 != 0) System.out.println(array[i]);
         }
